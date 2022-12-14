4bf21ff7974e20475727809c85484cd04f0cd9bd
pedrogrl <pedro.gos@outlook.com>
2022-12-14 13:13:31 -0800

    base components + db

---
+++ src/components/TodoEmpty.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-center text-lg text-gray-500">
+        Você ainda não tem nenhuma tarefa.
+    </div>
+</template>
+
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="space-y-2">
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'; 
+
+export default {
+    components: {TodoItem}
+}
+</script>
